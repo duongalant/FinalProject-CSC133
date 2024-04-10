e1cf5df0dea4257b9811283e5729b416a8d02065
alanduong <duongalant@gmail.com>
2024-04-09 19:28:01 -0700

    Uploading origin SnakeGame files

---
+++ app/src/main/java/com/gamecodeschool/c17snake/Apple.java
@@ -0,0 +1,49 @@
+package com.gamecodeschool.c17snake;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class Apple extends GameObject implements InSnake {
+    // The range of values we can choose from
+    // to spawn an apple
+    private Point mSpawnRange;
+    /// Set up the apple in the constructor
+    Apple(Context context, Point sr, int s){
+
+        // Make a note of the passed in spawn range
+        mSpawnRange = sr;
+        // Make a note of the size of an apple
+        mSize = s;
+        // Hide the apple off-screen until the game starts
+        location.x = -10;
+
+        // Load the image to the bitmap
+        mBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.apple);
+
+        // Resize the bitmap
+        mBitmap = Bitmap.createScaledBitmap(mBitmap, s, s, false);
+    }
+
+    // This is called every time an apple is eaten
+    void spawn(){
+        // Choose two random values and place the apple
+        Random random = new Random();
+        location.x = random.nextInt(mSpawnRange.x) + 1;
+        location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+    }
+    void spawn(ArrayList<Point> segmentLocations){
+        // Choose two random values and place the apple
+        Random random = new Random();
+        location.x = random.nextInt(mSpawnRange.x) + 1;
+        location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+
+        if(InSnake.checkSpot(segmentLocations, location, -1)) this.spawn(segmentLocations);
+    }
+}
